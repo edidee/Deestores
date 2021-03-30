d5e28d972786defdd17dbc60b4fc0949e65ae71f
edidee <etokedidionge@gmail.com>
2021-03-30 06:34:09 +0100

    added drawer

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -11,38 +11,75 @@
           tag="article"
           style="max-width: auto"
           class="mb-2 text-left font-size card"
+          @click="showDetails(product)"
+          v-b-toggle.sidebar-right
         >
-          <!-- <b-card-text class="text-left fs-3">
-            {{ product.description}}
-        </b-card-text> -->
           <b-card-text class="text-left price">
             US ${{ product.price }}
           </b-card-text>
-          <button
-            type="button"
-            class="btn btn-success"
-            @click="addToCart(product)"
-          >
+          <button type="button" class="btn" @click="addToCart(product)">
             Add to Cart
           </button>
-
-          <!-- <b-card-text class="text-left">
-          {{ product.category}}
-        </b-card-text> -->
         </b-card>
       </div>
-      <div>
-    <b-button v-b-toggle.sidebar-right>Toggle Sidebar</b-button>
-    <b-sidebar id="sidebar-right" title="Sidebar" right shadow>
-      <div class="px-3 py-2">
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
-          in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-        </p>
-        <b-img src="https://picsum.photos/500/500/?image=54" fluid thumbnail></b-img>
+
+      <div class="text-left" v-if="product">
+        <!-- <b-button >Toggle Sidebar</b-button> -->
+        <b-sidebar id="sidebar-right" :title="product.title" right shadow v-model="visible">
+          <div class="px-3 py-2">
+            <b-img :src="product.image" fluid thumbnail></b-img>
+            <p>
+              {{ product.description }}
+            </p>
+            <p class="price">US ${{ product.price }}</p>
+            <div class="d-flex justify-content-between">
+              <div style="width: auto; margin-right: 6%" class="mb-3 mt-2">
+                <button
+                  @click="increase"
+                  style="
+                    border-radius: 50%;
+                    border: 0;
+                    width: 20px;
+                    height: 25px;
+                  "
+                >
+                  +
+                </button>
+                &nbsp;&nbsp; <span :disabled="disablebtn">1</span>&nbsp;&nbsp;
+                <button
+                  @click="decrease"
+                  style="
+                    border-radius: 50%;
+                    border: 0;
+                    width: 20px;
+                    height: 25px;
+                  "
+                >
+                  -
+                </button>
+              </div>
+              <b-form inline class="mb-3">
+                <label class="mr-sm-2" for="inline-form-custom-select-pref"
+                  >Color</label
+                >
+                <b-form-select
+                  id="inline-form-custom-select-pref"
+                  class="mb-2 mr-sm-2 mb-sm-0"
+                  :options="[
+                    { text: 'Choose...', value: null },
+                    'Blue',
+                    'Pink',
+                    'Black',
+                  ]"
+                  :value="null"
+                >
+                </b-form-select>
+              </b-form>
+            </div>
+            <button type="button" class="btn">Add to Cart</button>
+          </div>
+        </b-sidebar>
       </div>
-    </b-sidebar>
-  </div>
     </div>
   </div>
 </template>
@@ -52,11 +89,17 @@ export default {
   data() {
     return {
       products: [],
-      cart:[],
+      product: null,
+      visible:true,
+      cart: [],
       url: "http://localhost:3000/products?_limit=20",
     };
   },
   methods: {
+    showDetails(product) {
+      this.product = product;
+      console.log(this.product);
+    },
     async fetchAPIData() {
       try {
         const response = await this.$http.get(this.url);
@@ -104,8 +147,9 @@ h4 {
   font-weight: 600;
 }
 
-.btn-success {
+.btn {
   background-color: #fb9811;
   border: 1px solid #fb9811;
+  color: #ffff;
 }
 </style>
