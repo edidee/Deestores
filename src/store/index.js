97f2f4b4b9ceb685f0303cd5d90fdeebe71a8cd2
edidee <etokedidionge@gmail.com>
2021-05-27 14:15:15 +0100

    update

--- src/store/index.js
+++ src/store/index.js
@@ -6,8 +6,18 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     counter: 1,
+    products:[],
+  },
+  getters: {
+    products: state => {
+      return state.products;
+    }
   },
   mutations: {
+    SET_Item(state, products) {
+      state.products = products
+    },
+
     increament(state) {
       state.counter++
     },
@@ -16,7 +26,16 @@ export default new Vuex.Store({
     }
   },
   actions: {
-    
+    async loadProducts({ commit }) {
+      try {
+        const response = await this.$http.get("/api/products");
+        this.products = response.data
+        commit('SET_Items', 'products')
+      }
+      catch (error) {
+        console.log(error);
+      }
+    }
   },
   modules: {},
 });
